44a29aa7557053e89c924e1d4adf2976c7930760
Robert Isoski <r@robiso.com>
2020-05-17 21:14:52 +0200

    Update and rename translation-dutch.php to translation-german.php

--- translation-german.php
+++ translation-german.php
@@ -3,7 +3,7 @@
 global $Wcms;
 
 $Wcms->addListener('settings', function ($args) {
-    $translations = array_map('str_getcsv', file(__DIR__ . '/nl_NL.csv'));
+    $translations = array_map('str_getcsv', file(__DIR__ . '/de.csv'));
 
     foreach ($translations as $replacement) {
         if (count($replacement) !== 2) {
